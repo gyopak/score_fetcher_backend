29ef81fdd1933d1ffb0f7c2dc0d5abbf95a44a56
gy0p4k <gyorgypakozdi@gmail.com>
2017-10-28 09:29:42 +0200

    feat(app): template rest get endpoint

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
-@app.route('/')
-def homepage():
 
-    return "hello"
+@app.route('/', methods=['GET'])
+def template_get_endpoint():
+    return jsonify({'status': "ok"})
+
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
-
