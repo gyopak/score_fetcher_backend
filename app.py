1a280286d1ccc5cf5e5e7858b4f49958ff7dfbfe
gy0p4k <gyorgypakozdi@gmail.com>
2017-10-31 20:12:29 +0100

    feat(app): enpoint to logs

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from flask import Flask, jsonify
+from src.logger import log, get_log_messages
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'])
 def template_get_endpoint():
+    log("endpoint visited")
     return jsonify({'status': "ok"})
 
+
+@app.route("/logs", methods=["GET"])
+def get_logs():
+    return jsonify(get_log_messages())
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
--- src/logger.py
+++ src/logger.py
@@ -1,15 +1,23 @@
 from datetime import datetime
+import json
 
 def generate(level, message):
-	log = {}
-	log["time"] = str(datetime.now())
-	log["level"] = level
-	log["message"] = message
-	return str(log)
+    log = {}
+    log["time"] = str(datetime.now())
+    log["level"] = level
+    log["message"] = message
+    return str(log)
+
 
 def save_log(log):
-	with open("log.txt", "a") as log_file:
-		log_file.write(log + "\n")
+    with open("log.txt", "a") as log_file:
+        log_file.write(log + ",\n")
 
-def log(message, level = "INFO"):
-	save_log(generate(level, message))
+
+def log(message, level="INFO"):
+    save_log(generate(level, message))
+
+
+def get_log_messages():
+    with open("log.txt", "r") as log_file:
+        return {"logs": log_file.read().split("\n")}
