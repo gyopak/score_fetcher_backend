4e3db6c63751fdea7b342687704c8214fccddf2f
gy0p4k <gyorgypakozdi@gmail.com>
2017-11-02 15:19:56 +0100

    feat(): add lots of dependencies

--- src/fetcher.py
+++ src/fetcher.py
@@ -1,2 +1,12 @@
-from settings import BASE_URL
+from settings import BASE_URL, GAME_TYPE
+from bs4 import BeautifulSoup
+import urllib
 
+
+def get_page_data(url):
+    with urllib.request.urlopen(url) as response:
+        return response.read()
+
+
+def make_soup():
+    return BeautifulSoup(get_page_data(BASE_URL + GAME_TYPE))
