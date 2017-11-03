1c9fd35e702c78cdb118416cb02d88b59e60984c
gy0p4k <gyorgypakozdi@gmail.com>
2017-11-03 02:22:51 +0100

    feat(soup): select groups, teams

--- settings.py
+++ settings.py
@@ -1,8 +1,14 @@
 BASE_URL = "https://www.eredmenyek.com/"
-GAME_TYPE = "jegkorong/"
+GAME_URL = "jegkorong/"
+GAME_TYPE = "hockey"
+
+SOUP_FORMAT =  "lxml"
 
 TEAM_HTML_ATTRIBUTE = "span"
+GROUP_HTML_ATTRIBUTE = "table"
+GROUP_COUNTY_CLASS = "country_part"
+GROUP_TOURNAMENT_CLASS = "tournament_part"
 TEAM_CLASS = "padl"
 
 
-SOUP_FORMAT =  "lxml"
+
--- src/external/soup.py
+++ src/external/soup.py
@@ -1,12 +1,27 @@
-from settings import BASE_URL, GAME_TYPE, SOUP_FORMAT
+from settings import BASE_URL, GAME_TYPE, SOUP_FORMAT, GAME_URL, GROUP_HTML_ATTRIBUTE, TEAM_HTML_ATTRIBUTE, GROUP_COUNTY_CLASS, GROUP_TOURNAMENT_CLASS
 from bs4 import BeautifulSoup
 import dryscrape
 
 
 def make_soup():
     session = dryscrape.Session()
-    session.visit(BASE_URL + GAME_TYPE)
+    session.visit(BASE_URL + GAME_URL)
     return BeautifulSoup(session.body(), SOUP_FORMAT)
 
-def match_making(raw):
-    pass    
+
+def get_matches():
+    raw = make_soup()
+    groups= raw.find_all(GROUP_HTML_ATTRIBUTE, {"class": GAME_TYPE})
+    matches = []
+    for group in groups:
+        matches.extend(get_match_by_group(group))
+    return matches
+
+def get_match_by_group(group):
+    country = group.find(TEAM_HTML_ATTRIBUTE, {"class": GROUP_COUNTY_CLASS})
+    tournament = group.find(TEAM_HTML_ATTRIBUTE, {"class": GROUP_TOURNAMENT_CLASS})
+    match_parts = group.findAll("tr", {"id" : lambda L: L})
+    home_teams = match_parts[::2]
+    away_teams = match_parts[1::2]
+    for home_team in home_teams:
+            
